7db508c01f21035ce25b7badb2c9e33de0e72fb6
myhifi <147342908+myhifi@users.noreply.github.com>
2026-03-22 02:30:50 +0200

    Add product routes to server

--- server.js
+++ server.js
@@ -9,18 +9,13 @@ app.use(express.json());
 
 // import routes
 const userRoutes = require("./routes/users_route");
+const productRoutes = require("./routes/products_route");
 
 // use Routes
 app.use("/users" , userRoutes);
-
+app.use("/products", productRoutes);
 
 // 3) Listen
 app.listen(5000 , ()=>{
     console.log("Server is Runnng on Port 5000");
 })
-
-// Task make another route:
-// products:
-//     id
-//     name
-//     salary(Price)
